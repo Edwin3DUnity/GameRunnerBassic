377e2348a572b4d7176a32488262c67b1fa3056e
EdwinUnity3D <eapaniagua8@misena.edu.co>
2024-02-18 21:16:30 -0500

    Generado script SpwanManger

--- Assets/_Scripts/RepeatBackground.cs
+++ Assets/_Scripts/RepeatBackground.cs
@@ -5,17 +5,27 @@ using UnityEngine;
 public class RepeatBackground : MonoBehaviour
 {
 
-    private float widthBackground;
+    private float mitadFondo;
+
+    private Vector3 posInicial;
     
+  
     // Start is called before the first frame update
     void Start()
     {
-        widthBackground = GetComponent<BoxCollider>().size.x / 2;
+        mitadFondo = GetComponent<BoxCollider>().size.x / 2;
+
+        posInicial = transform.position;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (posInicial.x - transform.position.x > mitadFondo)
+        {
+            transform.position = posInicial;
+        }
+
     }
 }
---
+++ Assets/_Scripts/SpawnManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] enemies;
+    private int indexEnemy;
+
+    private Vector3 spawnPos;
+
+
+    private float startSpawn = 1;
+
+    [SerializeField, Range(1, 3), Tooltip(" Tiempo de espera para el siguiente enemigo")]
+    private float nextEnemy = 2;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnPos = transform.position;
+        
+        InvokeRepeating("GenerarEnemigos", startSpawn, nextEnemy);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    private void GenerarEnemigos()
+    {
+        indexEnemy = Random.Range(0, enemies.Length);
+
+        Instantiate(enemies[indexEnemy], spawnPos, enemies[indexEnemy].transform.rotation);
+
+    }
+    
+}
