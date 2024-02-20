3b9218cb73cc002c3cb30136985fb75b5cc9eb90
EdwinUnity3D <eapaniagua8@misena.edu.co>
2024-02-20 14:36:05 -0500

    Creado PlayerController

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,68 +1,32 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 public class PlayerController : MonoBehaviour
 {
 
-    private Rigidbody _rigidbody;
-
-    [SerializeField, Range(1, 30), Tooltip("Fuerza de impulso hacia arriba para saltar")]
+    [SerializeField, Range(0, 20), Tooltip("Fuerza hacia arriba para saltar")]
     private float jumpForce = 8;
 
-    [SerializeField] private bool isOnGround; 
-    
-    private bool gameOver;
 
-    public bool GameOver
-    {
-        get => gameOver;
-        
-    }
+    private Rigidbody _rigidbody;
+    
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
-     Jump();   
+        Jump();
     }
 
 
     private void Jump()
     {
-        
-
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver )
-        {
-            _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
-            isOnGround = false;
-
-        }
+        _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
         
     }
-
-    private void OnCollisionEnter(Collision other)
-    {
-        if (other.gameObject.CompareTag("Ground"))
-        {
-            isOnGround = true;
-        }else if (other.gameObject.CompareTag("Obstacle"))
-        {
-            gameOver = true;
-            Debug.Log("Gamer Over");
-            
-        }
-        
-    }
-
-    
-    
-    
 }
