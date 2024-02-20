03e4267da174e68f2718993bd013f474a4a0f8c9
EdwinUnity3D <eapaniagua8@misena.edu.co>
2024-02-20 18:03:14 -0500

    Modificado script playercontroller animator

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -21,11 +21,22 @@ public class PlayerController : MonoBehaviour
         get => gameOver;
         
     }
+
+    private Animator _animator;
+
+    private const string SPEED_F = "Speed_f";
+    private const string SPEED_MULTIPLIER = "SpeedMultiplier";
+    private const string JUMP_TRIG = "Jump_trig";
+    
+    
+    
+    
     // Start is called before the first frame update
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody>();
-
+        _animator = GetComponent<Animator>();
+        _animator.SetFloat(SPEED_F, 1);
     }
 
     // Update is called once per frame
@@ -38,12 +49,13 @@ public class PlayerController : MonoBehaviour
     private void Jump()
     {
         
-
+        _animator.SetFloat(SPEED_MULTIPLIER, 1 + Time.time / 10);
         if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver )
         {
             _rigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
-
+            
+            _animator.SetTrigger(JUMP_TRIG);
         }
         
     }
